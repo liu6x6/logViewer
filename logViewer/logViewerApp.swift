d2684243f15055cff69b3367257fb3aac4805ebb
Liu xiaolong <xiaolong.liu@vorwerk.cn>
2026-05-29 13:20:50 +0800

    Initial Commit

---
+++ logViewer/logViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  logViewerApp.swift
+//  logViewer
+//
+//  Created by xiaolong on 2026/5/29.
+//
+
+import SwiftUI
+
+@main
+struct logViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
